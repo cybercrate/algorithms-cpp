265bdbe482e2b139d9ecac3d66757dd76c1102ef
Alexander Shavrov <alexander.shavrov@outlook.com>
2022-06-21 03:55:25 +0300

    Implemented Knuth-Morris-Pratt substring searching algorithm

---
+++ include/strings.h
@@ -0,0 +1,6 @@
+#ifndef WINGMANN_ALGORITHMS_STRINGS_H
+#define WINGMANN_ALGORITHMS_STRINGS_H
+
+#include "strings/knuth_morris_pratt.h"
+
+#endif // WINGMANN_ALGORITHMS_STRINGS_H
--- src/algorithms.cpp
+++ src/algorithms.cpp
@@ -1,3 +1,4 @@
 #include "containers.h"
 #include "search.h"
 #include "sorting.h"
+#include "strings.h"
