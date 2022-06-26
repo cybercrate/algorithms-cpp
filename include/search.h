25333da82d07d3d972fd239b428192d5d12bdc7b
Alexander Shavrov <alexander.shavrov@outlook.com>
2022-06-26 06:52:06 +0300

    Implemented recursive binary search algorithm

--- include/search.h
+++ include/search.h
@@ -2,6 +2,7 @@
 #define WINGMANN_ALGORITHMS_SEARCH_H
 
 #include "search/binary_search.h"
+#include "search/binary_search_recursive.h"
 #include "search/fibonacci_search.h"
 #include "search/jump_search.h"
 #include "search/linear_search.h"
---
+++ test/search/binary_search_recursive_test.cpp
@@ -0,0 +1,42 @@
+#include "search.h"
+#include "search_test_helpers.h"
+
+#include "gtest/gtest.h"
+
+using namespace wingmann::algorithms::search;
+
+TEST(binary_search_recursive, empty)
+{
+    EXPECT_EQ(std::nullopt, binary_search_recursive(std::vector<int>{}, 1));
+}
+
+TEST(binary_search_recursive, one_value)
+{
+    EXPECT_EQ(0, binary_search_recursive(std::vector<int>{64}, 64).value());
+}
+
+TEST(binary_search_recursive, several_values)
+{
+    std::vector<int> data;
+    int target;
+    int searched_target;
+
+    for (std::size_t i = 0; i < 100; i++) {
+        data = get_sorted_vector_with_random_values<int>();
+        target = get_value<int>(data);
+        searched_target = data[binary_search_recursive(data, target).value()];
+
+        EXPECT_EQ(target, searched_target);
+    }
+}
+
+TEST(binary_search_recursive, not_found)
+{
+    std::vector<int> data;
+    auto target = 1'001;
+
+    for (std::size_t i = 0; i < 100; i++) {
+        data = get_sorted_vector_with_random_values<int>();
+        EXPECT_EQ(std::nullopt, binary_search_recursive(data, target));
+    }
+}
