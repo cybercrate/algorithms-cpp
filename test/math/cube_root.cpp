c0481a1370a249742c372de89583f72e926db97f
Alexander Shavrov <alexander.shavrov@mail.ru>
2023-10-01 06:27:28 +0300

    Tests updated

--- test/math/cube_root.cpp
+++ test/math/cube_root.cpp
@@ -2,23 +2,22 @@
 #include <gtest/gtest.h>
 #include <string>
 
-
 TEST(algorithms_math, cube_root_1) {
-    auto x = cube_root(262'144);
+    auto x = wingmann::algorithms::math::cube_root(262'144);
     auto y = static_cast<int>(std::cbrt(262'144));
 
     EXPECT_EQ(std::to_string(x), std::to_string(y));
 }
 
 TEST(algorithms_math, cube_root_2) {
-    auto x = cube_root(2'126'781.656f);
+    auto x = wingmann::algorithms::math::cube_root(2'126'781.656f);
     auto y = std::cbrt(2'126'781.656f);
 
     EXPECT_EQ(std::to_string(x), std::to_string(y));
 }
 
 TEST(algorithms_math, cube_root_3) {
-    auto x = cube_root(35'611.289);
+    auto x = wingmann::algorithms::math::cube_root(35'611.289);
     auto y = std::cbrt(35'611.289);
 
     EXPECT_EQ(std::to_string(x), std::to_string(y));
